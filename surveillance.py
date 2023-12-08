bc5d4dff5df975473ffd196c013dd4ea1e87d460
AnthonyCoquelet <153290277+AnthonyCoquelet@users.noreply.github.com>
2023-12-08 16:05:19 +0100

    Update surveillance.py

--- surveillance.py
+++ surveillance.py
@@ -2,19 +2,24 @@ import os
 import psutil
 import time
 ospc= os.name
-fichier_Log = open("Monitoring.txt")
+fichier_Log = open("Monitoring.txt","a")
+monitoring = bool(True)
+slp = int(input("Combien de temps voulez-vous entre deux executions ?"))
 def os():
     if ospc == "nt":
-        fichier_Log.write("L'Os de l'ordinateur est Windows""\n")
+        fichier_Log.write("L'Os de l'ordinateur est Windows","\n")
     else :
-        fichier_Log.write("L'Os de l'ordinateur n'est pas Windows""\n")
+        fichier_Log.write("L'Os de l'ordinateur n'est pas Windows","\n")
 def cpu ():
+    
     x= psutil.cpu_percent(1)
-    fichier_Log.write("Le CPU est utiliser à {} %""\n".format(x))
+    fichier_Log.write("Le CPU est utiliser à {} %","\n".format(x))
 def ram():
     y=psutil.virtual_memory().percent
-    fichier_Log.write("La RAM est utiliser à {} %""\n".format(y))
+    fichier_Log.write("La RAM est utiliser à {} %","\n".format(y))
 
 os()
-cpu()
-ram()
+while monitoring == True :
+    time.sleep(slp)
+    cpu()
+    ram()
