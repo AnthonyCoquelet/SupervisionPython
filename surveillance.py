541627bca7c4c6a0e796b1e1705bb2e0bea2b96f
Just-Lea <153290269+Just-Lea@users.noreply.github.com>
2023-12-08 17:37:28 +0100

    Update surveillance.py

--- surveillance.py
+++ surveillance.py
@@ -3,29 +3,28 @@ import psutil
 import time
 import config
 import alrtEmail
-ospc= os.name
-fichier_Log = open((config.nom_Fichier,time.time),"a")
 monitoring = bool(True)
-def os():
-    if ospc == "nt":
-        fichier_Log.write("L'Os de l'ordinateur est Windows","\n")
-    else :
-        fichier_Log.write("L'Os de l'ordinateur n'est pas Windows","\n")
+fichier_Log = open(config.nom_Fichier,"a")
+
 def cpu ():
     x= psutil.cpu_percent(1)
-    fichier_Log.write("Le CPU est utiliser à {} %","\n".format(x))
     if x >= config.cpu:
         fichier_Log.write("Attention : Surcharge du processeur.")
+    else:
+        fichier_Log.write("Le CPU est utiliser à {} %".format(x))
+        fichier_Log.write("\n")
 def ram():
     y=psutil.virtual_memory().percent
-    fichier_Log.write("La RAM est utiliser à {} %","\n".format(y))
     if y >= config.ram:
-        fichier_Log.write("Attention : Surcharge de la RAM.")
+        fichier_Log.write("Attention : Surcharge du la RAM.")
+        fichier_Log.write("\n")
+    else:
+        fichier_Log.write("La RAM est utiliser à {} %".format(y))
+        fichier_Log.write("\n")
 
-os()
 while monitoring == True :
-    fichier_Log = open(config.nom_Fichier,time.time,"a")
+    fichier_Log = open(config.nom_Fichier,"a")
     time.sleep(config.slp)
     cpu()
     ram()
-    fichier_Log.close()
+    fichier_Log.close() 
