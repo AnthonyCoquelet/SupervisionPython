2392ab3d3a1d396e628d33beb99fcdad213fca60
AnthonyCoquelet <153290277+AnthonyCoquelet@users.noreply.github.com>
2023-12-08 15:52:30 +0100

    Update surveillance.py Fichier log

--- surveillance.py
+++ surveillance.py
@@ -5,15 +5,15 @@ ospc= os.name
 fichier_Log = open("Monitoring.txt")
 def os():
     if ospc == "nt":
-        fichier_Log.write("L'Os de l'ordinateur est Windows")
+        fichier_Log.write("L'Os de l'ordinateur est Windows""\n")
     else :
-        fichier_Log.write("L'Os de l'ordinateur n'est pas Windows")
+        fichier_Log.write("L'Os de l'ordinateur n'est pas Windows""\n")
 def cpu ():
     x= psutil.cpu_percent(1)
-    fichier_Log.write("Le CPU est utiliser à {} %".format(x))
+    fichier_Log.write("Le CPU est utiliser à {} %""\n".format(x))
 def ram():
     y=psutil.virtual_memory().percent
-    fichier_Log.write("La RAM est utiliser à {} %".format(y))
+    fichier_Log.write("La RAM est utiliser à {} %""\n".format(y))
 
 os()
 cpu()
