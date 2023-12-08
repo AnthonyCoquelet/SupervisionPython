c7be14a3f2dfdf9e1378f913e63deba75ffdadd6
AnthonyCoquelet <153290277+AnthonyCoquelet@users.noreply.github.com>
2023-12-08 15:41:28 +0100

    Ajout fichier log

--- surveillance.py
+++ surveillance.py
@@ -2,17 +2,18 @@ import os
 import psutil
 import time
 ospc= os.name
+fichier_Log = open("Monitoring.txt")
 def os():
     if ospc == "nt":
-        print("L'Os de l'ordinateur est Windows")
+        fichier_Log.write("L'Os de l'ordinateur est Windows")
     else :
-        print("L'Os de l'ordinateur n'est pas Windows")
+        fichier_Log.write("L'Os de l'ordinateur n'est pas Windows")
 def cpu ():
     x= psutil.cpu_percent(1)
-    print("Le CPU est utiliser à {} %".format(x))
+    fichier_Log.write("Le CPU est utiliser à {} %".format(x))
 def ram():
     y=psutil.virtual_memory().percent
-    print("La RAM est utiliser à {} %".format(y))
+    fichier_Log.write("La RAM est utiliser à {} %".format(y))
 
 os()
 cpu()
