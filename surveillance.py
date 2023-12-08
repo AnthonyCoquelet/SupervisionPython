7ab0e20101a76a75e3f4ab8899f48fada147c6f2
Just-Lea <153290269+Just-Lea@users.noreply.github.com>
2023-12-08 15:37:08 +0100

    Update surveillance.py

--- surveillance.py
+++ surveillance.py
@@ -1,6 +1,19 @@
 import os
+import psutil
+import time
 ospc= os.name
-if ospc == "nt":
-  print("L'Os de l'ordinateur est Windows")
-else :
-  print("L'Os de l'ordinateur n'est pas Windows")
+def os():
+    if ospc == "nt":
+        print("L'Os de l'ordinateur est Windows")
+    else :
+        print("L'Os de l'ordinateur n'est pas Windows")
+def cpu ():
+    x= psutil.cpu_percent(1)
+    print("Le CPU est utiliser à {} %".format(x))
+def ram():
+    y=psutil.virtual_memory().percent
+    print("La RAM est utiliser à {} %".format(y))
+
+os()
+cpu()
+ram()
